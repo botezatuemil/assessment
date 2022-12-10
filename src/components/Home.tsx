225c0ab2512d4ddf14abd2764b311ad0273ab6c7
Emil Botezatu <btzemil@gmail.com>
2022-12-10 12:25:53 +0200

    fetched the list of gists for searched username

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import qs from "qs";
 
 const Home = () => {
+
+  const [page, setPage] = useState<number>(1);
+  const [username, setUsername] = useState<string>("");
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    
+
+    getPublicGists();
   };
+
+  const getPublicGists = async () => {
+    const url = `https://api.github.com/users/${username}/gists`;
+
+    const options = {
+      per_page: "2",
+      page: page,
+    };
+
+    const queryString = qs.stringify(options);
+
+    try {
+      const { data } = await axios.get(`${url}?${queryString}`);
+      console.log(data);
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onChangeHandler = (event:React.FormEvent<HTMLInputElement> ) => {
+    setUsername(event.currentTarget.value);
+  }
 
   return (
     <div className="flex w-full h-full justify-center">
@@ -17,6 +47,7 @@ const Home = () => {
           <input
             className="w-[30vw] rounded bg-[#343941] text-white h-[30px] px-2"
             placeholder="Enter Github username"
+            onChange={onChangeHandler}
           />
         </div>
         <button
